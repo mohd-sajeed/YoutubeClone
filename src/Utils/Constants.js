15893cfdd13b2809ce8644a905c85924e24f9b3c
mohdsajeed <mohdsajeed871@gmail.com>
2024-04-25 18:01:17 +0530

    api updated

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -5,7 +5,7 @@ export const LIVE_CHAT_COUNT = 25;
 
 export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=In&key=" + GOOGLE_API_KEY;
 
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const YOUTUBE_VIDEO_WATCH_API = `https://www.googleapis.com/youtube/v3/videos?&part=snippet,contentDetails,statistics&key=${GOOGLE_API_KEY}&id=`
 
